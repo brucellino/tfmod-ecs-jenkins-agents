a6af10422a4196d40f52f42fa480ad578ac55294
Bruce Becker <brucellino@protonmail.com>
2022-06-27 17:58:06 +0200

    chore(providers): add http provider
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- terraform.tf
+++ terraform.tf
@@ -5,11 +5,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.19.0"
+      version = "4.20.0"
     }
-  }
-}
 
-provider "aws" {
-  # Configuration options
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.7.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "2.2.0"
+    }
+
+  }
 }
