80e55f0bf00db33b631cb2f43222025badf57c62
Bruce Becker <brucellino@protonmail.com>
2022-06-28 08:14:40 +0200

    feat(ecs)!: add FARGATE capacity provider to ecs cluster
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- ecs.tf
+++ ecs.tf
@@ -1,17 +1,34 @@
 resource "aws_ecs_cluster" "build_agents" {
   name = "${var.cluster_name}-${var.environment}"
+
 }
 
 # Create task definition for basic agent
 resource "aws_ecs_task_definition" "vanilla-agent" {
-  family                = "ecs-vanilla-${var.environment}"
-  container_definitions = file("${path.module}/task-definitions/ecs-vanilla.json")
-
+  family                   = "ecs-vanilla-${var.environment}"
+  container_definitions    = file("${path.module}/task-definitions/ecs-vanilla.json")
+  cpu                      = 512
+  memory                   = 1024
   network_mode             = "awsvpc"
-  requires_compatibilities = ["EC2"]
+  requires_compatibilities = ["FARGATE"]
 
   volume {
-    name      = "deploy"
-    host_path = "/mnt/efs/deploy"
+    name = "deploy"
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.ecs-build-artifacts.id
+      root_directory = "/mnt/efs/deploy"
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "fargate" {
+  cluster_name = aws_ecs_cluster.build_agents.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
   }
 }
